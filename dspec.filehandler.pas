5b2d8e3844c04342a238aac261971608e7fa3766
Geoffrey Smith <github@tysontechnology.com.au>
2024-01-07 12:16:01 +1100

    Cleanup old spec object usage

--- dspec.filehandler.pas
+++ dspec.filehandler.pas
@@ -7,19 +7,17 @@ uses
   DPM.Core.Logging,
   DPM.Core.Spec.Interfaces,
   DPM.Core.Spec.Reader,
-  DPM.Core.Spec.Writer,
-  dpm.dspec.format
+  DPM.Core.Spec.Writer
   ;
 
 
 type
   TDSpecFile = class
   private
-    FLoaded : TDPMSpecFormat;
     FLogger: ILogger;
     FReader: IPackageSpecReader;
     FFilename : string;
-    structure : TDPMSpecFormat;
+    FLoadedSpec : IPackageSpec;
   public
     spec : IPackageSpec;
     function NewTemplate(const templateName: string): ISpecTemplate;
@@ -65,17 +63,10 @@ uses
 
 function TDSpecFile.AddCompiler(const compiler: string): ISpecTargetPlatform;
 var
-//  platforms : TArray<TTargetPlatform>;
   vplatform : ISpecTargetPlatform;
 begin
   if Assigned(GetPlatform(compiler)) then
     raise Exception.Create('Platform already exists in file');
-//  platforms := structure.targetPlatforms;
-//  SetLength(platforms, Length(platforms) + 1);
-//  platforms[High(platforms)] := TTargetPlatform.Create;
-//  platforms[High(platforms)].compiler := compiler;
-//  structure.targetPlatforms := platforms;
-//  Result := platforms[High(platforms)];
 
   vplatform := TSpecTargetPlatform.Create(FLogger);
   vplatform.Compiler := StringToCompilerVersion(compiler);
@@ -84,82 +75,39 @@ begin
 end;
 
 function TDSpecFile.AsString: string;
-var
-  json : TJSONObject;
 begin
-  json := TJson.ObjectToJsonObject(structure);
-  try
-    Result := json.Format;
-  finally
-    FreeAndNil(json);
-  end;
+  Result := spec.ToJSON;
 end;
 
 constructor TDSpecFile.Create(logger: ILogger);
 begin
-  structure := TDPMSpecFormat.Create;
-  FLoaded := TDPMSpecFormat.Create;
   FLogger := logger;
   spec := TSpec.Create(FLogger, '');
+  FLoadedSpec := TSpec.Create(FLogger, '');
 end;
 
 procedure TDSpecFile.DeleteTemplate(const templateName: string);
-var
-  templates : TArray<TTemplate>;
-  templateNew : TArray<TTemplate>;
-  i, j: Integer;
 begin
-  templates := structure.templates;
-  SetLength(templateNew, Length(templates) - 1);
-  j := 0;
-
-  for i := 0 to High(structure.targetPlatforms) do
-  begin
-    if structure.targetPlatforms[i].template = templateName then
-    begin
-      structure.targetPlatforms[i].template := '';
-    end;
-  end;
-
-  for i := 0 to High(templates) do
-  begin
-    if templates[i].name <> templateName then
-    begin
-      templateNew[j] := templates[i];
-      Inc(j);
-    end;
-  end;
-
-  structure.templates := templateNew;
-
   spec.DeleteTemplate(templateName);
 end;
 
 procedure TDSpecFile.DeleteCompiler(const compiler: string);
 var
-  targetPlatforms : TArray<TTargetPlatform>;
-  targetPlatformsNew : TArray<TTargetPlatform>;
-  i, j: Integer;
+  i : Integer;
 begin
-  targetPlatforms := structure.targetPlatforms;
-  SetLength(targetPlatformsNew, Length(targetPlatforms) - 1);
-  j := 0;
-  for i := 0 to High(targetPlatforms) do
+  for i := 0 to spec.TargetPlatforms.Count - 1 do
   begin
-    if targetPlatforms[i].compiler <> compiler then
+    if SameText(CompilerToString(spec.TargetPlatforms[i].compiler), compiler) then
     begin
-      targetPlatformsNew[j] := targetPlatforms[i];
-      Inc(j);
+      spec.TargetPlatforms.Delete(i);
+      Exit;
     end;
   end;
-  structure.targetPlatforms := targetPlatformsNew;
 end;
 
 
 destructor TDSpecFile.Destroy;
 begin
-  FreeAndNil(structure);
-  FreeAndNil(FLoaded);
   inherited;
 end;
 
@@ -167,39 +115,12 @@ function TDSpecFile.DoesTemplateExist(const templateName: string): Boolean;
 var
   i : Integer;
 begin
-  Result := False;
-  for i := 0 to High(structure.templates) do
-  begin
-    if structure.templates[i].name = templateName then
-    begin
-      Result := True;
-      Exit;
-    end;
-  end;
+  Result := Assigned(spec.FindTemplate(templateName));
 end;
 
 function TDSpecFile.DuplicateTemplate(const sourceTemplate: ISpecTemplate; const newTemplateName: string): ISpecTemplate;
-var
-  json : TJSONObject;
 begin
-  { TODO : Complete this method }
-
-{  sourceTemplate
-  Result := nil;
-  json := TJson.ObjectToJsonObject(sourceTemplate);
-  try
-    templates := structure.templates;
-    SetLength(templates, length(templates) + 1);
-    templates[High(templates)] := TJson.JsonToObject<TTemplate>(json);
-    if not DoesTemplateExist(newTemplateName) then
-    begin
-      templates[High(templates)].name := newTemplateName;
-      structure.templates := templates;
-    end;
-    Result := templates[High(templates)];
-  finally
-    FreeAndNil(json);
-  end; }
+  Result := spec.DuplicateTemplate(sourceTemplate, NewTemplateName);
 end;
 
 function TDSpecFile.GetTemplate(const templateName: string): ISpecTemplate;
@@ -218,28 +139,7 @@ begin
 end;
 
 procedure TDSpecFile.RenameTemplate(const originalName: string; const newName: string);
-var
-  i : Integer;
 begin
-  if DoesTemplateExist(newName) then
-    raise Exception.Create('Template already Exists');
-  for i := 0 to High(structure.templates) do
-  begin
-    if SameText(structure.templates[i].name, originalName) then
-    begin
-      structure.templates[i].name := newName;
-    end;
-  end;
-
-  for i := 0 to High(structure.targetPlatforms) do
-  begin
-    if SameText(structure.targetPlatforms[i].template, originalName) then
-    begin
-      structure.targetPlatforms[i].template := newName;
-    end;
-  end;
-
-//
   spec.RenameTemplate(originalName, newName);
 end;
 
@@ -357,46 +257,23 @@ end;
 
 function TDSpecFile.IsModified: Boolean;
 begin
-  Result := TJSON.ObjectToJsonObject(structure).Format <> TJson.ObjectToJsonObject(FLoaded).Format;
+  Result := not SameText(spec.ToJSON, FLoadedSpec.ToJSON);
 end;
 
 procedure TDSpecFile.LoadFromFile(const filename: string);
-var
-  json : TJSONObject;
 begin
   FReader := TPackageSpecReader.Create(FLogger);
   spec := FReader.ReadSpec(filename);
-  //spec.MetaData.
-  json := TJSONObject.ParseJSONValue(TFile.ReadAllText(Filename)) as TJSONObject;
-  try
-    if not Assigned(json) then
-    begin
-      raise Exception.Create('Failed to load ' + filename);
-    end;
-
-//    structure := TJson.JsonToObject<TDPMSpecFormat>(json as TJSONObject);
-//    FLoaded := TJson.JsonToObject<TDPMSpecFormat>(json as TJSONObject);
-    FFilename := Filename;
-  finally
-    FreeAndNil(json);
-  end;
+  FLoadedSpec := FReader.ReadSpec(filename);
+  FFilename := Filename;
 end;
 
 procedure TDSpecFile.SaveToFile(const filename: string);
 var
-  json : TJSONObject;
   writer : TPackageSpecWriter;
 begin
   writer := TPackageSpecWriter.Create(FLogger, spec);
   writer.SaveToFile(filename);
-
-  json := TJson.ObjectToJsonObject(structure);
-  try
-//    TFile.WriteAllText(Filename, json.Format);
-    LoadFromFile(filename);
-  finally
-    FreeAndNil(json);
-  end;
 end;
 
 function TDSpecFile.WorkingDir: string;
