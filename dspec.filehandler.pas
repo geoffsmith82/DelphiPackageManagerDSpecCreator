930312ec179df4efe5fa073f6e310da9f6a226df
Geoffrey Smith <github@tysontechnology.com.au>
2024-01-08 22:21:19 +1100

    Remove unused functions

--- dspec.filehandler.pas
+++ dspec.filehandler.pas
@@ -20,20 +20,10 @@ type
     FLoadedSpec : IPackageSpec;
   public
     spec : IPackageSpec;
-    function NewTemplate(const templateName: string): ISpecTemplate;
     procedure DeleteTemplate(const templateName: string);
-    procedure RenameTemplate(const originalName: string; const newName: string);
     function DuplicateTemplate(const sourceTemplate: ISpecTemplate; const newTemplateName: string): ISpecTemplate;
     function DoesTemplateExist(const templateName: string): Boolean;
     function GetTemplate(const templateName: string): ISpecTemplate;
-    function NewSource(const templateName: string; srcPath: string): ISpecFileEntry;
-    function NewFile(const templateName: string; srcPath: string): ISpecFileEntry;
-    function NewLib(const templateName: string; srcPath: string): ISpecFileEntry;
-    function NewBuild(const templateName: string; BuildId: string): ISpecBuildEntry;
-    function NewDesign(const templateName, designSrc: string): ISpecBPLEntry;
-    function NewDependency(const templateName: string; DependencyId: string): ISpecDependency;
-    function NewRuntime(const templateName: string; const runtimeBuildId: string): ISpecBPLEntry;
-    function NewSearchPath(const templateName: string; const SearchPathId: string): ISpecSearchPath;
     function GetPlatform(const compiler: string): ISpecTargetPlatform;
     function AddCompiler(const compiler: string): ISpecTargetPlatform;
     procedure DeleteCompiler(const compiler: string);
@@ -114,8 +104,6 @@ begin
 end;
 
 function TDSpecFile.DoesTemplateExist(const templateName: string): Boolean;
-var
-  i : Integer;
 begin
   Result := Assigned(spec.FindTemplate(templateName));
 end;
@@ -138,138 +126,6 @@ begin
       Exit;
     end;
   end;
-end;
-
-procedure TDSpecFile.RenameTemplate(const originalName: string; const newName: string);
-begin
-  spec.RenameTemplate(originalName, newName);
-end;
-
-function TDSpecFile.NewTemplate(const templateName: string): ISpecTemplate;
-begin
-  Result := nil;
-  if templateName.IsEmpty then
-    Exit;
-
-  Result := spec.NewTemplate(templateName);
-end;
-
-function TDSpecFile.NewBuild(const templateName: string; BuildId: string): ISpecBuildEntry;
-var
-  template : ISpecTemplate;
-begin
-  Result := nil;
-  if BuildId.IsEmpty then
-    Exit;
-  if not DoesTemplateExist(templateName) then
-    raise Exception.Create('Template does not exist');
-
-  template := GetTemplate(templateName);
-
-  Result := template.NewBuildEntryById(BuildId);
-end;
-
-function TDSpecFile.NewDependency(const templateName: string; DependencyId: string): ISpecDependency;
-var
-  template : ISpecTemplate;
-begin
-  Result := nil;
-  if DependencyId.IsEmpty then
-    Exit;
-  if not DoesTemplateExist(templateName) then
-    raise Exception.Create('Template does not exist');
-
-  template := GetTemplate(templateName);
-  Result := template.NewDependencyById(DependencyId);
-end;
-
-function TDSpecFile.NewRuntime(const templateName: string; const runtimeBuildId: string): ISpecBPLEntry;
-var
-  template : ISpecTemplate;
-begin
-  Result := nil;
-  if runtimeBuildId.IsEmpty then
-    Exit;
-  if not DoesTemplateExist(templateName) then
-    raise Exception.Create('Template does not exist');
-
-  template := GetTemplate(templateName);
-  Result := template.NewRuntimeBplBySrc(runtimeBuildId);
-end;
-
-function TDSpecFile.NewDesign(const templateName: string; const designSrc: string): ISpecBPLEntry;
-var
-  template : ISpecTemplate;
-begin
-  Result := nil;
-  if designSrc.IsEmpty then
-    Exit;
-  if not DoesTemplateExist(templateName) then
-    raise Exception.Create('Template does not exist');
-
-  template := GetTemplate(templateName);
-
-  Result := template.NewDesignBplBySrc(designSrc);
-end;
-
-function TDSpecFile.NewSearchPath(const templateName: string; const SearchPathId: string): ISpecSearchPath;
-var
-  template : ISpecTemplate;
-begin
-  Result := nil;
-  if SearchPathId.IsEmpty then
-    Exit;
-  if not DoesTemplateExist(templateName) then
-    raise Exception.Create('Template does not exist');
-
-  template := GetTemplate(templateName);
-
-  Result := template.NewSearchPath(SearchPathId);
-end;
-
-function TDSpecFile.NewSource(const templateName: string; srcPath: string): ISpecFileEntry;
-var
-  template : ISpecTemplate;
-begin
-  Result := nil;
-  if srcPath.IsEmpty then
-    Exit;
-  if not DoesTemplateExist(templateName) then
-    raise Exception.Create('Template does not exist');
-
-  template := GetTemplate(templateName);
-
-  Result := template.NewSource(srcPath);
-end;
-
-function TDSpecFile.NewFile(const templateName: string; srcPath: string): ISpecFileEntry;
-var
-  template : ISpecTemplate;
-begin
-  Result := nil;
-  if srcPath.IsEmpty then
-    Exit;
-  if not DoesTemplateExist(templateName) then
-    raise Exception.Create('Template does not exist');
-
-  template := GetTemplate(templateName);
-
-  Result := template.NewFiles(srcPath);
-end;
-
-function TDSpecFile.NewLib(const templateName: string; srcPath: string): ISpecFileEntry;
-var
-  template : ISpecTemplate;
-begin
-  Result := nil;
-  if srcPath.IsEmpty then
-    Exit;
-  if not DoesTemplateExist(templateName) then
-    raise Exception.Create('Template does not exist');
-
-  template := GetTemplate(templateName);
-
-  Result := template.NewLib(srcPath);
 end;
 
 function TDSpecFile.GetPlatform(const compiler: string): ISpecTargetPlatform;
